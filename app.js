f8732571ea75422a5b112b809234400ae52c29c3
Aakash Chopra <chopra.aakash7@gmail.com>
2017-03-16 17:44:50 +0530

    Heroku && listen port

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-var app = require('express').app();
+var app = require('express')();
 
 app.get('/', (req, res) => {
     res.status(200).send('<html><body><h1>Aanchal is a fool.</h1></body></html>')
+});
+
+var port = process.env.port || 3000;
+
+app.listen(port, () => {
+    console.log(`Server up on port ${3000}`);
 });
