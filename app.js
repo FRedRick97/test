d5b0baa1d2d4c45054a8483c1aba87d045739ae2
Aakash <chopra.aakash7@gmail.com>
2017-03-16 17:56:07 +0530

    PORT FIX

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.get('/', (req, res) => {
     res.status(200).send('<html><body><h1>Aanchal is a fool.</h1></body></html>')
 });
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server up on port ${port}`);
